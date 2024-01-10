eea01e941099c4efda334d3464834b1e4a020178
au-lex <aulexc4d2@gmail.com>
2024-01-10 21:45:14 +0100

    installation

---
+++ src/Component/Header.jsx
@@ -0,0 +1,20 @@
+import { CgMenuRight } from "react-icons/cg";import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <header className='bg-red-500 w-full'>
+<div>
+    logo
+</div>
+<div><CgMenuRight /></div>
+
+
+  </header>
+
+    
+     </>
+  )
+}
+
+export default Header
---
+++ src/Component/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+    
+    
+    
+    Welcome to [Your Company's Name] Internship Program!
+Are you ready to kickstart your career with a transformative internship experience? At [Your Company's Name], we offer a dynamic and enriching program designed to empower aspiring professionals. Dive into real-world projects using cutting-edge technologies and develop your skills in HTML, CSS, and JavaScript. 
+Explore the possibilities, gain hands-on experience, and unlock doors to a bright future. Your journey begins here!
+    
+    </>
+  )
+}
+
+export default Hero
