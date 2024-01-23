eeed70bee196dd9978d9086c13a5967874d3039f
Aulex <117493012+au-lex@users.noreply.github.com>
2024-01-23 14:05:10 +0100

    Update Header.jsx

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -57,7 +57,7 @@ const Header = () => {
 
       {openMenu && (
         <div          data-aos="fade-up"
-        data-aos-duration="1400"  className={`bg-slate-900  ${openMenu ? 'open' : ''}  text-center menubar text-slate-100  mt-[5rem]  pt-8 px-4 w-full z-50
+        data-aos-duration="400"  className={`bg-slate-900  ${openMenu ? 'open' : ''}  text-center menubar text-slate-100  mt-[5rem]  pt-8 px-4 w-full z-50
          h-[10rem] fixed `}>
          <a href="#home" onClick={handleNavLinkClick}  className='block font-medium my-8'>Home</a>
   <a href="#about" onClick={handleNavLinkClick}  className='block font-medium  my-8'>About</a>
